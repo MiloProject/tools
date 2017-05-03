d2ec5326a4934e3019534edae8031bef1bf37bbd
Eliott T <developpsoft@gmail.com>
2017-05-03 08:58:50 +0200

    Premier draft

--- regexp.py
+++ regexp.py
@@ -1,1 +1,11 @@
 # Permet de tester les expressions régulières de la base de données
+import re
+
+def teste(regexp, phrase):
+  if re.match(regexp, phrase):
+    print("MATCH")
+  else:
+    print("NOPE")
+    
+if __nhame__ == "__main__":
+  teste(input("REGEXP > "), input("TEST > "))
